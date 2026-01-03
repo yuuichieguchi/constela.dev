48791919eeaf628aff5b595aa3f875922b489173
Yuuichi <ichi.wen@icloud.com>
2026-01-03 15:57:23 +0900

    update: libraries' version

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,10 +1,10 @@
-export const REPO_DOCS = 'https://github.com/constela/constela.dev';
-export const REPO_MAIN = 'https://github.com/constela/constela';
+export const REPO_DOCS = "https://github.com/yuuichieguchi/constela.dev";
+export const REPO_MAIN = "https://github.com/yuuichieguchi/constela";
 
 export const siteConfig = {
-  name: 'Constela',
-  description: 'A compiler-first UI language for vibecoding',
-  url: 'https://constela.dev',
+  name: "Constela",
+  description: "A compiler-first UI language for vibecoding",
+  url: "https://constela.dev",
   links: {
     github: REPO_MAIN,
     docs: REPO_DOCS,
